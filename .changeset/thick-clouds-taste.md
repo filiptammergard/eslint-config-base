b726324cad9e8c8be0d7231b1d5655b7e32a5233
Filip Tammergård <filip@tammergard.se>
2024-04-16 16:18:31 +0200

    Simplify

---
+++ .changeset/thick-clouds-taste.md
@@ -0,0 +1,5 @@
+---
+"@tammergard/eslint-config-base": minor
+---
+
+Simplify a lot.
--- index.js
+++ index.js
@@ -1,35 +1,11 @@
-const { FlatCompat } = require("@eslint/eslintrc")
 const globals = require("globals")
 const js = require("@eslint/js")
 
-const compat = new FlatCompat({
-	baseDirectory: __dirname,
-})
-
 module.exports = [
 	js.configs.recommended,
-	...compat.extends("eslint-config-airbnb-base"),
-	...compat.extends("plugin:eslint-plugin-prettier/recommended"),
 	{
 		rules: {
-			"import/extensions": [
-				"error",
-				"never",
-				{ svg: "always", json: "always", png: "always" },
-			],
-			"import/no-unresolved": "off",
-			"import/prefer-default-export": "off",
 			"no-use-before-define": "off",
-			"import/no-extraneous-dependencies": [
-				"error",
-				{
-					devDependencies: [
-						"**/*.test.{ts,tsx}", // test files
-						"**/eslint.config.{js,mjs,cjs}", // eslint flat config
-						"**/.prettierrc.{js,mjs,cjs}", // prettier config
-					],
-				},
-			],
 		},
 	},
 	{
